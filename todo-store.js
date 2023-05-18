e2ffda137fa3f8c81b5175a38e3b237200f649b0
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-05-18 16:21:40 +0200

    add todo store

---
+++ todo-store.js
@@ -0,0 +1,7 @@
+const todos = [
+    {name: 'Geburi von Sarah', description: 'Anrufen', priority: "*****", duedate: 'in a day', status: "Open"},
+    {name: 'Ferien', description: 'Flug buchen', priority: "***", duedate: 'in 7 days', status: "Open"},
+    {name: 'Einkaufen', description: 'Pizza Schuhe Kekse', priority: "*", duedate: 'a day ago', status: "Open"},
+]
+
+export default {todos};
