e5e9ad2af90468a7f750c81e34ac9b8694a1450c
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-05-21 14:46:54 +0200

    hide and show detail view

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ const themeButton = document.getElementById("theme-button");
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 });
+
+const switchView = document.querySelector(".switch-view");
+const overview = document.querySelector(".overview");
+const newentry = document.querySelector(".new-entry");
+
+switchView.addEventListener("click", () => {
+  overview.classList.toggle("hide");
+  newentry.classList.toggle("hide");
+});
+
