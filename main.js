2a5ff5ff0f1f11d14c4409a2d51452b21b678b7d
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-05-21 17:01:28 +0200

    fix switch view bug

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 });
 
-const switchView = document.querySelector(".switch-view");
+const switchViewButtons = document.querySelectorAll(".switch-view");
 const overview = document.querySelector(".overview");
 const newentry = document.querySelector(".new-entry");
 
-switchView.addEventListener("click", () => {
-  overview.classList.toggle("hide");
-  newentry.classList.toggle("hide");
-});
+for (let i = 0; i < switchViewButtons.length; i++) {
+  switchViewButtons[i].addEventListener("click", () => {
+    overview.classList.toggle("hide");
+    newentry.classList.toggle("hide");
+  });
+}
 
