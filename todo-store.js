e7a9628247d1d4d9688039e659c8518483225e6a
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-05-21 12:18:10 +0200

    add first version of sorting and filters

--- todo-store.js
+++ todo-store.js
@@ -1,7 +1,7 @@
 const todos = [
-    {name: 'Geburi von Sarah', description: 'Anrufen', priority: "*****", duedate: 'in a day', status: "Open"},
-    {name: 'Ferien', description: 'Flug buchen', priority: "***", duedate: 'in 7 days', status: "Open"},
-    {name: 'Einkaufen', description: 'Pizza Schuhe Kekse', priority: "*", duedate: 'a day ago', status: "Open"},
+    {created: 1684663897851, name: 'Geburi von Sarah', description: 'Anrufen', priority: "*****", duedate: 'in a day', status: true},
+    {created: 1684663897850, name: 'Ferien', description: 'Flug buchen', priority: "**", duedate: 'in 7 days', status: false},
+    {created: 1684663897859, name: 'Einkaufen', description: 'Pizza Schuhe Kekse', priority: "***", duedate: 'a day ago', status: false},
 ]
 
 export default {todos};
