1caed3d01cc96da63ea534033144a2999743aa29
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-05-21 17:00:43 +0200

    change duedate to date

--- todo-store.js
+++ todo-store.js
@@ -1,7 +1,7 @@
 const todos = [
-    {created: 1684663897851, name: 'Geburi von Sarah', description: 'Anrufen', priority: "*****", duedate: 'in a day', status: true},
-    {created: 1684663897850, name: 'Ferien', description: 'Flug buchen', priority: "**", duedate: 'in 7 days', status: false},
-    {created: 1684663897859, name: 'Einkaufen', description: 'Pizza Schuhe Kekse', priority: "***", duedate: 'a day ago', status: false},
+    {created: 1684663897851, name: 'Geburi von Sarah', description: 'Anrufen', priority: "*****", duedate: "2023-05-21", status: true},
+    {created: 1684663897850, name: 'Ferien', description: 'Flug buchen', priority: "**", duedate: "2023-03-13", status: false},
+    {created: 1684663897859, name: 'Einkaufen', description: 'Pizza Schuhe Kekse', priority: "***", duedate: "2023-06-11", status: false},
 ]
 
 export default {todos};
