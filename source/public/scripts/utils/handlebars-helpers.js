388351453a74eec07e48ad7666afb7b6ce9271f2
philippschnueriger <philipp.schnueriger@hotmail.com>
2023-06-11 23:20:13 +0200

    add handlebars helpers

---
+++ source/public/scripts/utils/handlebars-helpers.js
@@ -0,0 +1,32 @@
+Handlebars.registerHelper("times", (n, block) => {
+  let accum = "";
+  for (let i = 0; i < n; ++i) accum += block.fn(i);
+  return accum;
+});
+
+Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
